742f26af8f1851f5f210a7ad801ed43145b853ab
Joel May <git@jmay.us>
2022-12-17 21:07:22 -0800

    Add support for drawing readings to the Inky Impression display

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-from inky.auto import auto
-from homeassistant_api import Client
-import yaml
-import time
 import datetime
+import time
+from typing import List, Optional
+import yaml
+from inky.auto import auto
+from inky.inky_uc8159 import Inky # Assumes Inky Impression
+from homeassistant_api import Client
+from PIL import Image, ImageDraw, ImageFont
+
+FONT = "DejaVuSans.ttf"
+FONT_SIZE_UPDATED_AT = 25
+FONT_SIZE_SENSOR_NAME = 30
+FONT_SIZE_SENSOR_VALUE = 80
+MARGIN = (10, 10)
+
+FONT_UPDATED_AT = ImageFont.truetype(FONT, FONT_SIZE_UPDATED_AT)
+FONT_SENSOR_NAME = ImageFont.truetype(FONT, FONT_SIZE_SENSOR_NAME)
+FONT_SENSOR_VALUE = ImageFont.truetype(FONT, FONT_SIZE_SENSOR_VALUE)
+
+COLOR_UPDATED_AT = 'black'
+COLOR_SENSOR_NAME = 'black'
+COLOR_SENSOR_VALUE = 'black'
+
+class SensorDefinition:
+    def __init__(self, entity_id: str, name: str) -> None:
+        self.entity_id = entity_id
+        self.name = name
+
+    def read(self, ha_client: Client) -> 'SensorReading':
+        entity = ha_client.get_entity(entity_id = self.entity_id)
+        val = entity.state.state
+        unit = entity.state.attributes['unit_of_measurement']
+        return SensorReading(self.name, val, unit)
+
+class SensorReading:
+    def __init__(self, name: str, value: str, unit: str) -> None:
+        self.name = name
+        self.value = value
+        self.unit = unit
+
+    def formatted_value(self) -> str:
+        return '%s %s' % (self.value, self.unit)
 
 def main() -> None:
     print('Starting Inky Home')
@@ -28,28 +65,57 @@ def main() -> None:
         conf['homeassistant']['access_token']
     )
 
-    main_loop(client, conf)
+    sensors: List[SensorDefinition] = []
+    for sensor in conf['display']:
+        sensors.append(SensorDefinition(sensor['entity_id'], sensor['name']))
 
-def main_loop(ha_client, conf) -> None:
+    main_loop(client, display, sensors)
+
+def main_loop(ha_client: Client, inky_display: Optional[Inky], sensors: List[SensorDefinition]) -> None:
     while True:
-        print()
-        for sensor in conf['display']:
+        readings: List[SensorReading] = []
+        for sensor in sensors:
             try:
-                display_sensor(ha_client, sensor)
+                readings.append(sensor.read(ha_client))
             except Exception as ex:
                 print('Update error: ' + ex)
-        display_now()
+        display_readings(readings, inky_display)
         time.sleep(5 * 60)
 
-def display_sensor(client: Client, sensor) -> None:
-    entity = client.get_entity(entity_id=sensor['entity_id'])
-    val = entity.state.state
-    unit = entity.state.attributes['unit_of_measurement']
-    print('%s: %s %s' % (sensor['name'], val, unit))
+def format_updated_at(updated_at: datetime.datetime) -> str:
+    return 'Updated at: ' + updated_at.strftime('%H:%M')
 
-def display_now() -> None:
-    now = datetime.datetime.now()
-    print('Updated at: ' + now.strftime('%H:%M'))
+def display_readings(readings: List[SensorReading], inky_display: Optional[Inky]):
+    updated_at = datetime.datetime.now()
+    display_readings_stdout(updated_at, readings)
+    display_readings_inky(updated_at, readings, inky_display)
+
+def display_readings_stdout(updated_at: datetime.datetime, readings: List[SensorReading]):
+    print(format_updated_at(updated_at))
+    for reading in readings:
+        print('%s: %s %s' % (reading.name, reading.value, reading.unit))
+
+def display_readings_inky(updated_at: datetime.datetime, readings: List[SensorReading], inky_display: Optional[Inky]):
+    if inky_display is None:
+        return
+    str_updated = format_updated_at(updated_at)
+
+    img = Image.new('RGB', inky_display.resolution, 'white')
+    draw = ImageDraw.Draw(img)
+
+    bbox = draw.textbbox((0, 0), str_updated, FONT_UPDATED_AT)
+    offset = (MARGIN[0], img.height - MARGIN[1] - bbox[3])
+    draw.text(offset, str_updated, COLOR_UPDATED_AT, FONT_UPDATED_AT)
+
+    offset = MARGIN
+    for reading in readings:
+        draw.text(offset, reading.name, COLOR_SENSOR_NAME, FONT_SENSOR_NAME)
+        offset = (offset[0], draw.textbbox(offset, reading.name, FONT_SENSOR_NAME)[3])
+        draw.text(offset, reading.formatted_value(), COLOR_SENSOR_VALUE, FONT_SENSOR_VALUE)
+        offset = (offset[0], draw.textbbox(offset, reading.formatted_value(), FONT_SENSOR_VALUE)[3])
+
+    inky_display.set_image(img)
+    inky_display.show(busy_wait=False)
 
 if __name__ == '__main__':
     main()
